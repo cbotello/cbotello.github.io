2e11805bda3c1f92b37eaab03f7dcf2e44726d13
cbotello <carlosbotello@Mac-mini-de-Carlos.local>
2026-01-03 00:17:21 -0600

    code to enable inputs on factura.html

--- projects/QRcodes/js/factura.js
+++ projects/QRcodes/js/factura.js
@@ -8,6 +8,18 @@ var inputNumero = document.getElementById('inputNumero');
 var inputCliente = document.getElementById('inputCliente');
 var inputTotal = document.getElementById('inputTotal');
 
+var inputRFC= document.getElementById('rfc');
+var inputRazonSocial = document.getElementById('razonSocial');
+var inputRegimen = document.getElementById('regimenFiscal');
+var inputCFDI = document.getElementById('usoCFDI');
+
 inputNumero.value = numero;
 inputCliente.value = cliente.replace('_', ' ');
 inputTotal.value = `$ ${total}`;
+
+function habilitarCampos(){
+    inputRFC.disabled = false;
+    inputRazonSocial.disabled = false;
+    inputRegimen.disabled = false;
+    inputCFDI.disabled = false;
+}
